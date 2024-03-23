3db187215a9f318c4d6cb35dc6aa164f5148ed60
Yauheni Abramovich <yauheni.abramovich@appcast.io>
2024-03-23 14:11:59 -0300

    asiodjksa31 23123

---
+++ publisher.js
@@ -0,0 +1,6 @@
+console.log(process.args);
+console.log('publisher.js');
+console.log('module.exports:', module?.exports);
+console.log('exports:', exports);
+console.log('this:', this);
+console.log(process.env);
