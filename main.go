fcb3df179e1047fd9ec30b38456303927c26a0a2
Yauheni Abramovich <yauheni.abramovich@appcast.io>
2024-03-23 12:30:15 -0300

    dasjhdkjas

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "runtime"
+
+func main() {
+	println("This is a test program to check the runtime environment of the Go program.")
+	println("GOOS: ", runtime.GOOS)
+	println("GOARCH: ", runtime.GOARCH)
+	println("Version: ", runtime.Version())
+}
